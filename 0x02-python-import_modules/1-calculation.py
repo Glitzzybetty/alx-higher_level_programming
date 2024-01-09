61640eee0dc226047c1f5e7871f905cd1a5a80f2
Glitzzybetty <omotayo.ikudayisi@gmail.com>
2024-01-09 04:19:42 -0800

    a function that mimics calculator when imported

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Add, subtract, multiply and divide the values of a and b,
-    then print """
+    
     from calculator_1 import add, sub, mul, div
 
     a = 10
